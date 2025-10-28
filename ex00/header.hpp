10eee7a4fed13b1259eed975eb2bddeebac569e3
João <jocolognesi@gmail.com>
2025-10-27 21:35:47 -0300

    ex00

--- ex00/header.hpp
+++ ex00/header.hpp
@@ -2,7 +2,29 @@
 #ifndef HEADER_HPP
 #define HEADER_HPP
 
-#include <string>
 #include <iostream>
+
+template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp; 
+}
+
+template <typename T>
+T& min(T& a, T& b) {
+	if (a < b) {
+		return a;
+	}
+	return b;
+}
+
+template <typename T>
+T& max(T& a, T& b) {
+	if (a > b) {
+		return a;
+	}
+	return b;
+}
 
 #endif
